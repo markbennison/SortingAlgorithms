b388045585f7b0e44e06c3e6da9f5ea4e0d59c1b
Mark Bennison <markbennison@users.noreply.github.com>
2024-09-25 12:49:20 +0100

    Add project files.

---
+++ MenuSystem.cs
@@ -0,0 +1,85 @@
+﻿using System.Reflection;
+
+namespace SortingAlgorithms
+{
+    internal class MenuSystem
+    {
+        string callingClass;
+        Dictionary<string, MethodInfo> menuOptions;
+
+        public MenuSystem(string callingClassName)
+        {
+            callingClass = callingClassName;
+
+            menuOptions = new Dictionary<string, MethodInfo>();
+
+            Type thisType = typeof(Environment);
+            AddMenuItem("Exit Application", thisType.GetMethod("Exit"));
+        }
+
+        public void RunForever()
+        {
+            // Infinite loop (until application exited)
+            while (true)
+            {
+                Run();
+            }
+        }
+
+        public void Run()
+        {
+            DisplayOptions();
+            GetMenuSelection();
+        }
+
+        void DisplayOptions()
+        {
+            Console.Clear();
+            Console.WriteLine("Please choose from the following options: \n");
+            int i = 0;
+
+            foreach (string menuItem in menuOptions.Keys)
+            {
+                Console.WriteLine("(" + i + ") " + menuItem);
+                i++;
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        void GetMenuSelection()
+        {
+
+            string userInput = Console.ReadLine();
+
+            MethodInfo mi;
+
+            // Has the user entered an integer value?
+            if (int.TryParse(userInput, out int integerInput))
+            {
+                if (integerInput == 0)
+                {
+                    mi = menuOptions.ElementAt(integerInput).Value;
+                    mi.Invoke(this, new object[] { 0 });
+                    Console.ReadKey();
+                }
+                else if (integerInput > 0 && integerInput < menuOptions.Count)
+                {
+                    mi = menuOptions.ElementAt(integerInput).Value;
+                    mi.Invoke(this, null);
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        public void AddMenuItem(string menuMessage, MethodInfo methodInfo)
+        {
+            menuOptions.Add(menuMessage, methodInfo);
+        }
+
+        public void AddMenuItem(string menuMessage, string methodName, Type callingType)
+        {
+            MethodInfo methodInfo = callingType.GetMethod(methodName);
+            menuOptions.Add(menuMessage, methodInfo);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿/* *************************************************************************** *
+ * *************************************************************************** *
+ *       __  ___           __       ____                   _                   *
+ *      /  |/  /___ ______/ /__    / __ )___  ____  ____  (_)________  ____    *
+ *     / /|_/ / __ `/ ___/ //_/   / __  / _ \/ __ \/ __ \/ / ___/ __ \/ __ \   *
+ *    / /  / / /_/ / /  / ,<     / /_/ /  __/ / / / / / / (__  ) /_/ / / / /   *
+ *   /_/  /_/\__,_/_/  /_/|_|   /_____/\___/_/ /_/_/ /_/_/____/\____/_/ /_/    *
+ *                                                                             *
+ * *************************************************************************** *
+ * *************************************************************************** */
+
+using System.Reflection;
+
+namespace SortingAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            MenuSystem mainmenu = new MenuSystem("Program");
+
+            mainmenu.AddMenuItem("one", "One", typeof(Program));
+            mainmenu.AddMenuItem("two", "Two", typeof(Program));
+            mainmenu.AddMenuItem("three", "Three", typeof(Program));
+
+            mainmenu.RunForever();
+
+        }
+
+        public static void One()
+        {
+            Console.WriteLine("ONE WORLD");
+        }
+
+        public static void Two()
+        {
+            Console.WriteLine("TWO WORLD");
+        }
+
+        public static void Three()
+        {
+            Console.WriteLine("THREE WORLD");
+        }
+
+    }
+}
