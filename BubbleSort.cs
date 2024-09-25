fe7a1bea4027ef238bb9df3b1c8d37b6649e7f87
Mark Bennison <markbennison@users.noreply.github.com>
2024-09-25 13:12:04 +0100

    Added Bubble Sort

---
+++ BubbleSort.cs
@@ -0,0 +1,43 @@
+﻿/* *************************************************************************** *
+ * *************************************************************************** *
+ *       __  ___           __       ____                   _                   *
+ *      /  |/  /___ ______/ /__    / __ )___  ____  ____  (_)________  ____    *
+ *     / /|_/ / __ `/ ___/ //_/   / __  / _ \/ __ \/ __ \/ / ___/ __ \/ __ \   *
+ *    / /  / / /_/ / /  / ,<     / /_/ /  __/ / / / / / / (__  ) /_/ / / / /   *
+ *   /_/  /_/\__,_/_/  /_/|_|   /_____/\___/_/ /_/_/ /_/_/____/\____/_/ /_/    *
+ *                                                                             *
+ * *************************************************************************** *
+ * *************************************************************************** */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    internal class BubbleSort
+    {
+        public static void Perform(List<double> listToSort)
+        {
+            bool swapped = true;
+            double temp;
+
+            while (swapped)
+            {
+                swapped = false;
+                for (int i = 0; i < listToSort.Count - 1; i++)
+                {
+                    if (listToSort[i] > listToSort[i + 1])
+                    {
+                        temp = listToSort[i];
+                        listToSort[i] = listToSort[i + 1];
+                        listToSort[i+1] = temp;
+                        swapped = true; 
+                    }
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,38 +9,64 @@
  * *************************************************************************** *
  * *************************************************************************** */
 
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace SortingAlgorithms
 {
     internal class Program
     {
+        static Random randomGenerator = new Random();
+        static List<double> listOfDoubles = new List<double>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TenDoubles();
             MenuSystem mainmenu = new MenuSystem("Program");
 
-            mainmenu.AddMenuItem("one", "One", typeof(Program));
-            mainmenu.AddMenuItem("two", "Two", typeof(Program));
-            mainmenu.AddMenuItem("three", "Three", typeof(Program));
+            mainmenu.AddMenuItem("Randomise List of Doubles (10 Values)", "TenDoubles", typeof(Program));
+            mainmenu.AddMenuItem("Print List of Doubles", "PrintListOfDoubles", typeof(Program));
+            mainmenu.AddMenuItem("Perform a Bubble Sort", "PerformBubble", typeof(Program));
 
             mainmenu.RunForever();
-
+            Console.ReadKey();
         }
 
-        public static void One()
+        public static void PerformBubble()
         {
-            Console.WriteLine("ONE WORLD");
+            Console.WriteLine("Performing Bubble Sort...");
+            BubbleSort.Perform(listOfDoubles);
+            Console.WriteLine("Sort Completed");
         }
 
-        public static void Two()
+        public static void TenDoubles()
         {
-            Console.WriteLine("TWO WORLD");
+            Console.WriteLine("Populating list with random numbers....");
+            PopulateListWithRandomDoubles(10);
+            Console.WriteLine("List populated");
         }
 
-        public static void Three()
+        static void PopulateListWithRandomDoubles(int size)
         {
-            Console.WriteLine("THREE WORLD");
+            listOfDoubles.Clear();
+
+            for (int i = 0; i < size; i++)
+            {
+                double twoDigitDouble = Double.Parse(randomGenerator.NextDouble().ToString("0.00000"));
+                listOfDoubles.Add(twoDigitDouble);
+            }
+        }
+
+        public static void PrintListOfDoubles()
+        {
+            Console.WriteLine("\n\nLIST PRINT:\n");
+
+            for (int i = 0; i < listOfDoubles.Count; i++)
+            {
+                Console.WriteLine("  " + i + "  " + listOfDoubles[i].ToString());
+            }
+
+            Console.WriteLine("\nEND \n");
         }
 
     }
